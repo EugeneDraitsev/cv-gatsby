406a0197e1d075135569191c9dd031578f765c92
edraitsev <ddrrai@gmail.com>
2019-09-26 22:15:44 +0200

    Fixed start-url issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       options: {
         name: config.siteTitle,
         short_name: config.siteTitle,
-        start_url: config.pathPrefix,
+        start_url: '/',
         background_color: config.background_color,
         theme_color: config.theme_color,
         display: config.display,
