766545f3b0e3426ef8d5183fa3011f36ae757611
edraitsev <ddrrai@gmail.com>
2019-09-26 14:44:00 +0200

    Minor header adjustments

--- src/components/hero.component.tsx
+++ src/components/hero.component.tsx
@@ -9,6 +9,7 @@ const HeroContainer = styled.div<{ heroImg?: string }>`
   display: table;
   width: 100%;
   height: 70vh;
+  max-height: 500px;
   background-image: ${(p) => `url(${p.heroImg || withPrefix(siteConfig.siteCover)})`};
   background-attachment: fixed;
   background-position: center;
@@ -28,6 +29,7 @@ const HeroTitle = styled.h1`
   margin: 10px 60px;
   color: #fff;
   text-shadow: 1px 1px 4px rgba(34, 34, 34, 0.6);
+  user-select: none;
 `
 
 interface HeroProps {
