6fa54e230c77f5a55c0e8e760ae9c4b0498d4fdd
Eugene Draitsev <Eugene.Draitsev@ebuilder.com>
2019-09-26 12:51:56 +0200

    Added theme wrapper for ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,15 @@
+import React from 'react'
+import { ThemeProvider } from './src/contexts'
+
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+// eslint-disable-next-line react/prop-types
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider>
+    {element}
+  </ThemeProvider>
+)
