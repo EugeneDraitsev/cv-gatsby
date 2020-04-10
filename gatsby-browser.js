32f5e8fbd8c55e4079b113db3e25038e8bfe7078
Eugene Draitsev <ddrrai@gmail.com>
2020-04-10 10:18:03 +0200

    Updated libs to latest versions

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,3 @@
-import React from 'react'
-import { ThemeProvider } from './src/contexts/theme.context'
-
 export const onServiceWorkerUpdateReady = () => {
   window.location.reload()
 }
-
-// eslint-disable-next-line react/prop-types
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>
-    {element}
-  </ThemeProvider>
-)
---
+++ src/layouts/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ThemeProvider } from '../contexts'
+
+export default ({ children }: any) => (
+  <ThemeProvider>
+    {children}
+  </ThemeProvider>
+)
